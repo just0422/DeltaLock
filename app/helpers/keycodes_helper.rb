5243dd8b496776e4d6f3c6003d7c1662cdff10ae
Justin <just0422@gmail.com>
2016-07-06 17:08:59 -0400

    Created initial Keycodes Controller and Views

---
+++ app/helpers/keycodes_helper.rb
@@ -0,0 +1,2 @@
+module KeycodesHelper
+end
---
+++ test/controllers/keycodes_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class KeycodesControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
