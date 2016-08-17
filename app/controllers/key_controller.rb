6824542ce70643eda2b3deb817b8cb02b38570da
justin <just0422@hotmail.com>
2016-08-17 11:59:56 -0400

    Key codes looks pretty
    Key codes input and output fields fade as necessary
    Data stays consistent
    Ajax works for keys
    
    TODO:
    	Distribute editing everywhere else (POs and Purchasers)
    	Clean Nav bar
    	Mockup map GUI
    	Determine key hash
    	Allow relationships

--- app/controllers/key_controller.rb
+++ app/controllers/key_controller.rb
@@ -1,9 +1,21 @@
 class KeyController < ApplicationController
-    def show
-        print("***PARAMS")
-        print(params[:key_hash])
-        print("\n")
-        @key = Key.find(params[:id])
+	before_action :set_key, only: [:show, :edit, :update]
+	respond_to :html, :js
+
+	def show
         @pok = PoK.find_by_key_id(@key[:key_hash])
     end
+
+	def update
+		@key.update_attributes(key_params)
+	end
+
+	private
+	def key_params
+		params.require(:key).permit(:key, :master_key, :control_key, :stamp_code)
+	end
+
+	def set_key
+		@key = Key.find(params[:id])
+	end
 end
