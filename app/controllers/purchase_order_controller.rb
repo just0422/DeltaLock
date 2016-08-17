e6ce1df618cd12730753a3351a584534d16f581c
justin <just0422@hotmail.com>
2016-08-17 14:04:51 -0400

    Purchaser and Purchase order look good
    All input fields and output fields fade out accordingly
    All AJAX requests complete correctly
    
    TODO:
    	Clean Nav bar
    	Mockup Map GUI
    	Determin Key hash
    	Allow Relationships

--- app/controllers/purchase_order_controller.rb
+++ app/controllers/purchase_order_controller.rb
@@ -1,6 +1,8 @@
 class PurchaseOrderController < ApplicationController
-    def show
-        @purchaseorder = PurchaseOrder.find(params[:id])
+	before_action :set_purchaseorder, only: [:show, :edit, :update]
+	respond_to :html, :js
+	
+	def show
         @purchaser = Purchaser.find(@purchaseorder[:purchaser_id])
         @end_user = EndUser.find(@purchaseorder[:end_user_id])
         
@@ -11,4 +13,17 @@ class PurchaseOrderController < ApplicationController
             @keys_list.push(Key.find(po_k[:key_id]))
         end
     end
+
+	def update
+		@purchaseorder.update_attributes(purchaseorder_params)
+	end
+
+	private
+	def purchaseorder_params
+		params.require(:purchase_order).permit(:po_unmber, :date_order, :so_number)
+	end
+
+	def set_purchaseorder
+		@purchaseorder = PurchaseOrder.find(params[:id])
+	end
 end
--- app/controllers/purchaser_controller.rb
+++ app/controllers/purchaser_controller.rb
@@ -1,6 +1,22 @@
 class PurchaserController < ApplicationController
-    def show 
-        @purchaser = Purchaser.find(params[:id])
+	before_action :set_purchaser, only: [:show, :edit, :update]
+	respond_to :html, :js
+
+	def show 
         @purchase_orders_list = PurchaseOrder.where("purchaser_id like ?", "%#{params[:id]}%")
     end
+
+	def update
+		@purchaser.update_attributes(purchaser_params)
+	end
+
+
+	private
+	def purchaser_params
+		params.require(:purchaser).permit(:name, :address, :email, :phone, :fax)
+	end
+
+	def set_purchaser
+        @purchaser = Purchaser.find(params[:id])
+	end
 end
