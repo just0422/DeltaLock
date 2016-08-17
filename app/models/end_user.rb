06468229823052ea71ae8bf44e1c9446641ca8df
Justin <just0422@gmail.com>
2016-08-17 14:55:20 -0400

    Added Basic Map functionality
    Added mappable params to EndUser
    Added Map Controller for map section

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -1,3 +1,10 @@
 class EndUser < ActiveRecord::Base
     belongs_to :group
+
+    acts_as_mappable :default_units => :miles,
+                     :default_formula => :sphere,
+                     :distance_field_name => :distance,
+                     :lat_column_name => :lat,
+                     :lng_column_name => :lng
+
 end
