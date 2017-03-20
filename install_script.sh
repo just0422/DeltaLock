39239ef7064041b437735333b801ba7ab409c176
DeltaLock <delta@deltalock.biz>
2017-03-20 16:51:48 -0700

    Fixing up install script

--- install_script.sh
+++ install_script.sh
@@ -3,17 +3,18 @@
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
 GREEN='\033[0;32m'
+BLUE='\033[0;36m'
 NC='\033[0m'
 
 
-echo "Running apt-get update"
-apt-get update -yqq
+echo -e "${BLUE}Running apt-get update${NC}"
+apt-get -yq update
 
 pkgs=(git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs)
 
 for pkg in "${pkgs[@]}"
 do
-	echo -e "${NC}Installing $pkg"
+	echo -e "${BLUE}Installing $pkg"
 	if dpkg -s "$pkg" >/dev/null 2>&1; then
 		echo -e "${YELLOW}$pkg already installed"
 	elif apt-get -qq -y install "$pkg" >/dev/null 2>&1; then
@@ -22,13 +23,12 @@ do
 		echo -e "\t${RED}$pkg not installed"
 	fi
 done
-echo -e "${NC}Done"
 
 pkgs=(autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev)
 
 for pkg in "${pkgs[@]}"
 do
-	echo -e "${NC}Installing $pkg"
+	echo -e "${BLUE}Installing $pkg"
 	if dpkg -s "$pkg" >/dev/null 2>&1; then
 		echo -e "${YELLOW}$pkg already installed"
 	elif apt-get -qq -y install "$pkg" >/dev/null 2>&1; then
@@ -37,29 +37,43 @@ do
 		echo -e "\t${RED}$pkg not installed"
 	fi
 done
-echo -e "${NC}Done"
 
 
-
+echo -e "${BLUE}Config git${NC}"
 git config --global color.ui true
 git config --global user.name "DeltaLock"
 git config --global user.email "delta@deltalock.biz"
 
 cd ~
+echo -e "${BLUE}Removing previous .rbenv directory (if it exists)${NC}"
+rm -rf .rbenv
+echo -e "${BLUE}Cloning rbenv repository${NC}"
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo -e "${BLUE}Installing rbenv${NC}"
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-exec $SHELL
+if source ~/.bashrc; then
+	echo -e "${GREEN}rbenv successfully installed${NC}"
+else
+	echo -e "${RED}rbenv not installed${NC}"
+fi
 
+echo -e "${BLUE}Cloning ruby build into rbenv${NC}"
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
-exec $SHELL
+echo -e "${BLUE}Installing ruby${NC}"
+source ~/.bashrc
 
 rbenv install 2.4.0
 rbenv global 2.4.0
-ruby -v
+if ruby -v; then
+	echo -e "${GREEN}Ruby successfully installed${NC}"
+else
+	echo -e "${RED}Ruby not installed. Abort...${NC}"
+	exit 1
+fi
 
-
+exit 0
 gem install bundler
 
 
