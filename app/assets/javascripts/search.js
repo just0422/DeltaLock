5eb08d7d827a6e432470e1abb69ce65270d25c06
justin <just0422@hotmail.com>
2017-08-20 11:58:27 -0400

    Dynamic text fields works.... (.ready.....)

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,16 +2,18 @@
 //# All this logic will automatically be available in application.js.
 //# You can use CoffeeScript in this file: http://coffeescript.org/
 //
-$('form').on('click', '.remove_fields', function(event) {
-    $(this).closest('.field').remove();
-    return event.preventDefault();
-});
-$('form').on('click', '.add_fields', function(event) {
-    var regexp, time;
-    time = new Date().getTime();
-    regexp = new RegExp($(this).data('id'), 'g');
-    $(this).before($(this).data('fields').replace(regexp, time));
-    return event.preventDefault();
+$(document).ready(function() {
+	$('form').on('click', '.remove_fields', function(event) {
+		$(this).closest('.field').remove();
+		return event.preventDefault();
+	});
+	$('form').on('click', '.add_fields', function(event) {
+		var regexp, time;
+		time = new Date().getTime();
+		regexp = new RegExp($(this).data('id'), 'g');
+		$(this).before($(this).data('fields').replace(regexp, time));
+		return event.preventDefault();
+	});
 });
 
 function openTab(evt, searchtabName) {
