7ff8d08c5157a856269a4e610f76e0daffacb191
justin <just0422@hotmail.com>
2017-10-29 17:04:29 -0400

    Removed po_ks and added a Relationship table

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,84 +10,79 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170928040455) do
+ActiveRecord::Schema.define(version: 20171029193637) do
 
-  create_table "end_users", force: :cascade do |t|
+  create_table "end_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
-    t.string   "name",                 limit: 255
-    t.string   "phone",                limit: 255
-    t.string   "fax",                  limit: 255
-    t.string   "group_name",           limit: 255
-    t.string   "department",           limit: 255
-    t.integer  "store_number",         limit: 4
-    t.string   "primary_contact",      limit: 255
-    t.string   "primary_contact_type", limit: 255
-    t.string   "sub_department_1",     limit: 255
-    t.string   "sub_department_2",     limit: 255
-    t.string   "sub_department_3",     limit: 255
-    t.string   "sub_department_4",     limit: 255
+    t.string   "name"
+    t.string   "phone"
+    t.string   "fax"
+    t.string   "group_name"
+    t.string   "department"
+    t.integer  "store_number"
+    t.string   "primary_contact"
+    t.string   "primary_contact_type"
+    t.string   "sub_department_1"
+    t.string   "sub_department_2"
+    t.string   "sub_department_3"
+    t.string   "sub_department_4"
     t.float    "lat",                  limit: 24
     t.float    "lng",                  limit: 24
     t.text     "address",              limit: 65535
+    t.index ["id"], name: "index_end_users_on_id", using: :btree
   end
 
-  create_table "keys", force: :cascade do |t|
+  create_table "keys", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
-    t.string   "keyway",        limit: 255
-    t.string   "master_key",    limit: 255
-    t.string   "control_key",   limit: 255
-    t.string   "operating_key", limit: 255
-    t.string   "bitting",       limit: 255
-    t.string   "system_name",   limit: 255
+    t.string   "keyway"
+    t.string   "master_key"
+    t.string   "control_key"
+    t.string   "operating_key"
+    t.string   "bitting"
+    t.string   "system_name"
     t.text     "comments",      limit: 4294967295
+    t.index ["id"], name: "index_keys_on_id", using: :btree
   end
 
-  create_table "po_ks", force: :cascade do |t|
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "quantity",          limit: 4
-    t.integer  "key_id",            limit: 4
-    t.integer  "purchase_order_id", limit: 4
-  end
-
-  add_index "po_ks", ["key_id"], name: "index_po_ks_on_key_id", using: :btree
-  add_index "po_ks", ["purchase_order_id"], name: "index_po_ks_on_purchase_order_id", using: :btree
-
-  create_table "purchase_orders", force: :cascade do |t|
-    t.integer "po_number",    limit: 4
-    t.integer "so_number",    limit: 4
+  create_table "purchase_orders", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer "po_number"
+    t.integer "so_number"
     t.date    "date_order"
-    t.integer "purchaser_id", limit: 4
-    t.integer "end_user_id",  limit: 4
+    t.integer "purchaser_id"
+    t.integer "end_user_id"
+    t.index ["id"], name: "index_purchase_orders_on_id", using: :btree
   end
 
-  add_index "purchase_orders", ["end_user_id"], name: "index_purchase_orders_on_end_user_id", using: :btree
-  add_index "purchase_orders", ["purchaser_id"], name: "index_purchase_orders_on_purchaser_id", using: :btree
-
-  create_table "purchasers", force: :cascade do |t|
+  create_table "purchasers", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
-    t.string   "name",                 limit: 255
-    t.string   "phone",                limit: 255
-    t.string   "fax",                  limit: 255
-    t.string   "primary_contact",      limit: 255
-    t.string   "primary_contact_type", limit: 255
-    t.string   "group_name",           limit: 255
+    t.string   "name"
+    t.string   "phone"
+    t.string   "fax"
+    t.string   "primary_contact"
+    t.string   "primary_contact_type"
+    t.string   "group_name"
     t.text     "address",              limit: 65535
+    t.index ["id"], name: "index_purchasers_on_id", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.string   "first_name",      limit: 255
-    t.string   "last_name",       limit: 255
-    t.string   "username",        limit: 255
-    t.string   "password_digest", limit: 255
-    t.string   "role",            limit: 255
+  create_table "relationship", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.integer "purchase_order_id"
+    t.integer "purchaser_id"
+    t.integer "end_user_id"
+    t.integer "key_id"
   end
 
-  add_foreign_key "purchase_orders", "end_users"
-  add_foreign_key "purchase_orders", "purchasers"
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "username"
+    t.string   "password_digest"
+    t.string   "role"
+  end
+
 end
