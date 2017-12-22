f393dc99007c28e3cb8be37c7a7102d54b277ea6
DeltaLock <delta@deltalock.biz>
2017-12-22 00:49:40 -0500

    Small fix to Search page

--- db/migrate/20171029210613_create_relationships.rb
+++ db/migrate/20171029210613_create_relationships.rb
@@ -1,6 +1,5 @@
 class CreateRelationships < ActiveRecord::Migration[5.0]
   def change
-	drop_table :relationship
     create_table :relationships do |t|
       t.integer :purchase_order_id
       t.integer :purchaser_id
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171029210613) do
-
-  create_table "end_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
+create_table "end_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci" do |t|
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
     t.string   "name"
