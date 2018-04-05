05623c2c4af8c767329db1f836ee77d5544228a5
Justin <jmaldonado@cleverdevices.com>
2018-04-05 13:53:22 -0400

    Small style modification to search

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,6 @@ module ApplicationHelper
 	  fields = f.send("#{type}_fields", new_object, child_index: id) do |builder|
 		render("#{type}_fields", f: builder)
 	  end
-	  link_to(raw(name), '#', class: "add_fields btn green", data: {id: id, fields: fields.gsub("\n", "")})
+      link_to(raw(name), '#', class: "add_fields btn-floating green", data: {id: id, fields: fields.gsub("\n", "")})
 	end
 end
