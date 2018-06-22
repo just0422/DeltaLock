e58827577862d5519f7e7a2853b627f7f51af9b7
Justin <just0422@gmail.com>
2018-06-22 18:37:23 -0400

    Consolidated tables into one file

--- config/routes.rb
+++ config/routes.rb
@@ -8,34 +8,33 @@ Rails.application.routes.draw do
     root to: "home_page#index"
 
     get '' => 'home_page#index'
-    get '/show/:type/:id', to: 'entry#show', as: 'show_entry'
-    get '/edit/:type/:id', to: 'entry#edit', as: 'edit_entry'
-    get '/new', to: 'entry#new', as: 'new_entry'
+
+    get '/entry/show/:type/:id', to: 'entry#show', as: 'show_entry'
+    get '/entry/edit/:type/:id', to: 'entry#edit', as: 'edit_entry'
+    get '/entry/new', to: 'entry#new', as: 'new_entry'
+    post '/entry/create/:type', to: 'entry#create', as: 'create_entry'
+    post '/entry/update/:type/:id', to: 'entry#update', as: 'update_entry'
+    delete '/entry/delete/:type/:id', to: 'entry#delete', as: 'delete_entry'
+
+    get '/search', to: 'search#index'
+    post '/search/export/:search_type' => 'search#export', as: 'export'
+    post '/search/result' => 'search#items', as: 'search_result'
+    
+    get '/assign', to: 'assign#index'
     get '/assign/new/:type', to: 'assign#new', as: 'new_assign'
     get '/assign/search/:type', to: 'assign#search', as: 'search_assign'
     get '/assign/manage', to: 'assign#manage'
     get '/assign/edit/:id', to: 'assign#edit', as: 'edit_assign'
     get '/assign/filter/map', to: 'assign#filter_map', as: 'filter_map'
-    get '/manage', to: 'manage#index'
-
-    post '/search/export/:search_type' => 'search#export', as: 'export'
-    post '/update/:type/:id', to: 'entry#update', as: 'update_entry'
     post '/assign/create/:type', to: 'assign#create', as: 'create_assign'
     post '/assign/result/:type', to: 'assign#result', as: 'result_assign'
     post '/assign/update/:id', to: 'assign#update', as: 'update_assign'
     post '/assign/enduser', to: 'assign#session_enduser', as: 'session_enduser'
     post '/assign/update_map', to: 'assign#update_map', as: 'update_map'
     post '/assign/assignment', to: 'assign#assignment'
+    delete '/assign/delete/:id', to: 'assign#delete', as: 'delete_assign'
+
+    get '/manage', to: 'manage#index'
     post '/manage/upload', to: 'manage#upload', as: 'upload_manage'
     post '/manage/download/:template/:type', to: 'manage#download', as: 'download'
-    post '/create/:type', to: 'entry#create', as: 'create_entry'
-
-    delete '/delete/:type/:id', to: 'entry#delete', as: 'delete_entry'
-    delete '/delete/:id', to: 'assign#delete', as: 'delete_assign'
-
-    resources :search do
-        collection { post :result, to: 'search#items' }
-    end
-    resources :assign
-    resources :upload
 end
