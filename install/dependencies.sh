e69748c6c3ccdc9b1047ca04793a7c6bc2fd7b7b
Justin <just0422@gmail.com>
2018-07-10 11:19:43 -0400

    Cleaned up install text

--- install/dependencies.sh
+++ install/dependencies.sh
@@ -7,7 +7,8 @@ BLUE='\033[0;36m'
 NC='\033[0m'
 
 echo -e "${BLUE}Running apt-get update${NC}"
-apt-get -yq update
+apt-get -yq update > /dev/null
+echo -e "${GREEN}Packages updated sucessfully${NC}"
 
 pkgs=(git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev nodejs)
 
